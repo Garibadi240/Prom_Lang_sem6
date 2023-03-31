588b80d4810619416286d67b51245fd508ad5871
Danila Kosolapov <XO43210@gmail.com>
2023-03-31 21:27:38 +0300

    Работа выполнена

---
+++ Task41/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Clear();
+
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int number = GetNumberFromUser("Введите количество чисел: ", "Ошибка ввода!");
+
+int[] array = GetArray(number);
+
+Console.WriteLine(String.Join(" ", array));
+
+int count =  NumbersGreaterZero(array);
+
+Console.WriteLine($"Вы ввели чисел больше 0: {count}");
+
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int[] GetArray(int size){
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = GetNumberFromUser("Введите число: ", "Ошибка ввода!");
+    }
+    return res;
+}
+
+int NumbersGreaterZero(int[] arr){
+    int count = 0;
+    foreach (var el in arr)
+    {
+        if (el > 0) count ++;
+    }
+    return count;
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.Clear();
+
+/* Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.*/
+
+int number = 2;
+int number1 = 0;
+int number2 = 1;
+
+Console.WriteLine("Введите коэффициенты a1 и b1 линейной функции y=f(x):");
+double[] array1 = GetArray(number);
+
+Console.WriteLine("Введите коэффициенты a2 и b2 линейной функции y=g(x):");
+double[] array2 = GetArray(number);
+
+double[] arr = FindingIntersectionPoint(array1, array2);
+
+Console.WriteLine($"Точка пересечения: ({arr[0]}, {arr[1]})");
+
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+
+double[] GetArray(int size){
+    double[] res = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = GetNumberFromUser("Вы ввели чисело: ", "Ошибка ввода!");
+    }
+    return res;
+}
+
+double[] FindingIntersectionPoint(double[] arr1, double[] arr2){
+    double pointX = (arr2[number2]-arr1[number2])/(arr1[number1]-arr2[number1]);
+    double pointY = arr1[number1]*pointX+arr1[number2];
+    
+    double[] arr = new double[] {pointX, pointY};
+
+    return arr;
+}
